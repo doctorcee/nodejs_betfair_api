cf8edf707cce9b59ab50d9b3b02364c31e0ffbfa
doctorcee <oddsvantage@gmail.com>
2022-02-11 14:09:17 +0000

    2022-02-11 Add keepAlive and listMarketCatalogue functions to the NG module

--- api_ng/betfairapi.js
+++ api_ng/betfairapi.js
@@ -1,12 +1,12 @@
 #!/usr/bin node
 
-/* 
- * IMPORTANT - PLEASE READ THE LICENSE TERMS BEFORE 
- * DECIDING IF YOU WISH TO MAKE USE OF THIS CODE
- * 
- * Description
- * Module file with functions that invoke Betfair API operations
-*/ 
+//------------------------------------------------------------------------
+// IMPORTANT - PLEASE READ THE LICENSE TERMS BEFORE 
+// DECIDING IF YOU WANT TO USE THIS CODE
+//------------------------------------------------------------------------
+// Description
+// Module file with functions that invoke Betfair API operations
+//------------------------------------------------------------------------
 
 "use strict";
 
@@ -14,6 +14,9 @@
 var fs = require('fs');
 var url = require('url'); 
 var https = require('https'); 
+var zlib = require('zlib');
+
+const DEFAULT_ENCODING = 'utf-8';
 
 module.exports = {
     
@@ -49,8 +52,8 @@ module.exports = {
         //    c. login_params.ak - string storing the application key
         //    d. login_params.un - string storing account username
         //    e. login_params.pw - string storing account password
-        // 2. callback - callback function that accepts an object that contains
-        //    the following data:
+        // 2. callback - callback function which gets passed an object with 
+        //    the following items:
         //    a. response_status_code - integer HTTP status code.
         //    b. login_status - string containing the loginStatus string returned by API response
         //    c. session_id - string containing the session token value returned by API response
@@ -159,5 +162,182 @@ module.exports = {
             callback_params.error_message = ("Login Failure. Reason: HTTPS request error (" + e.message + ")");            
             callback(callback_params);    
         });
+    },
+
+    keepAlive : function(session_id,app_key,callback) {
+        // Input parameters:
+        // 1. session ID 	- current session ID
+        // 2. app key    	- account application key
+        // 3. callback 		- callback function which gets passed an object with
+        //    the following items:
+        //    a. Session token
+        //	  b. Status
+        //    c. Error_message
+
+        // Create the callback function parameter object that will be passed to callback 
+        // when we invoke it. This occurs on receipt of API login response, or an error.
+        let callback_params = {};
+        callback_params.status = '';
+        callback_params.token = '';
+        callback_params.error_message = '';
+
+        // Create URL object from the logon endpoint using the URL package
+        // See this link:  https://nodejs.org/api/url.html#url_url 
+        // for more details.
+        const ka_endpoint = 'https://identitysso.betfair.com/api/keepAlive';//"https://identitysso-cert.betfair.com/api/keepAlive";
+        let options = url.parse(ka_endpoint);
+
+        // Set the header and options - including application key and the 
+        // key file and cert file
+        options.method = 'POST';
+        options.port = 443;
+
+        // Set the application key and session token within the headers according to betfair documentation
+        options.headers = {
+			'Accept': 'application/json',
+			'X-Authentication' : session_id,
+            'X-Application': app_key
+        };
+
+        // Create a new https request object.
+        let req = https.request(options, function(res) {
+            // Create string to store the API response data
+            let responseData = "";
+            res.on('data', function(new_data) {
+                // New data arrived - append to existing string buffer
+                responseData += new_data;
+            });
+            res.on('end', function() {
+                // On end handler. Fires when the response has been fully received
+                try
+                {
+                    // Parse the response JSON
+                    let response = JSON.parse(responseData);
+                    const status = response.status;
+                    const response_code = res.statusCode;
+                    callback_params.status = response.status;
+					callback_params.token = response.token;
+					callback_params.error_message = response.error;
+                    callback(callback_params); 
+                }
+                catch (e)
+                {
+                    // JSON parser error - report the reason that the JSON was not
+                    // correctly parsed.
+                    callback_params.error_message = ("KA JSON parse failed. Reason: Exception caught (" + e.message + ")");
+                    callback(callback_params);    
+                }
+            });
+            res.on('error', function(e) {
+                // Error with response 
+                callback_params.error_message = ("KA Failure. Reason: HTTPS response error (" + e.message + ")");                
+                callback(callback_params);    
+            });
+        });
+
+        req.on('error', function(e) {
+            // Error with the request
+            callback_params.error_message = ("KA Failure. Reason: HTTPS request error (" + e.message + ")");            
+            callback(callback_params);    
+        });     
+        // post the request   
+        req.end();
+        console.log("Keep alive called.");
+    },
+    
+    listMarketCatalogue : function(session_id,app_key,market_filter,use_gzip_compression,callback) {
+        // Input parameter params:
+        // 1. Valid sesssion token
+        // 2. Valid application key
+        // 3. Market filter to apply to this request
+        // 4. Flag to indicate whether to request response is gzip compressed or not.
+        // 5. Callback function that accepts object that must contain the following objects:
+        //    a. error - a boolean error flag
+        //    b. error_message - string containing error details or "OK" when no error
+        //    c. data - string containing the JSON response
+        //    d. session_id - string duplicated from the input session token value        
+        
+        let https_options = {
+            hostname: 'api.betfair.com',
+            port: 443,
+            path: '/exchange/betting/json-rpc/v1',
+            agent: new https.Agent(),
+            method: 'POST',
+            headers: {            
+                'Accept': 'application/json',
+                'Content-type' : 'application/json',
+                'X-Authentication' : session_id,
+                'Connection':'Keep-Alive',
+                'X-Application' : app_key,
+            }
+        }
+        if (use_gzip_compression === true)
+		{
+			https_options.headers['Accept-Encoding'] = 'gzip';
+		}
+        
+        let response_params = {};
+        response_params.error = true;
+        response_params.error_message = 'ERROR';
+        response_params.data = '';
+        response_params.session_id = session_id;        
+        
+        let json_request = '{"jsonrpc":"2.0","method":"SportsAPING/v1.0/listMarketCatalogue", "params": ' + market_filter + ', "id": 1}';
+        
+        // Create a string buffer to store the response we get back
+        let response_buffer = '';
+        
+        // Create the HTTPS request now
+        let req = https.request(https_options,function (res) {
+			if (res.headers['content-encoding'] === 'gzip')
+			{
+				// Result is compressed
+				var gunzip = zlib.createGunzip();
+				gunzip.on('data', function(data) {
+					response_buffer += data.toString();
+				});
+				gunzip.on('end', function(){
+					response_params.data = response_buffer;
+					response_params.error = false;
+					response_params.error_message = "OK";
+					callback(response_params);
+				});
+				res.pipe(gunzip);
+			}
+			else
+			{
+				res.setEncoding(DEFAULT_ENCODING);
+				res.on('data', function (chunk) {
+					// Event handler for arrival of new data
+					// Append the new data to the buffer
+					response_buffer += chunk;
+				});
+				res.on('end', function() {
+					// Event handler for end of data received.
+					// When the transmission has ended we call the response
+					// parser function
+					response_params.data = response_buffer;
+					response_params.error = false;
+					response_params.error_message = "OK";
+					callback(response_params);
+				});
+			}
+			res.on('close', function(err) {
+				// Socket close error handler                
+                response_params.error_message = 'ERROR SOCKET CONNECTION CLOSED!';
+                response_params.data = '';
+                callback(response_params);
+			});			 
+        });
+            
+        // Send Json request object
+        req.write(json_request, DEFAULT_ENCODING);
+        req.end();
+        req.on('error', function(e) {
+            // error handler for request            
+            response_params.error_message = 'REQUEST ERROR: ' + e.message;
+            response_params.data = '';
+            callback(response_params);
+        });         
     }
 }
