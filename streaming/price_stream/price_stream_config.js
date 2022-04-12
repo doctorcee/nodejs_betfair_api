091661aabfdf5a999336f297583cc60fb382ee33
doctorcee <oddsvantage@gmail.com>
2022-04-12 10:45:19 +0100

    2022-04-12 Change config back to get horse markets

--- streaming/price_stream/price_stream_config.js
+++ streaming/price_stream/price_stream_config.js
@@ -3,13 +3,12 @@ let config = {
   priceStreamMonitorRate : 500,
   priceStreamHeartbeat : 2000,
   priceLadderLevels : 10,  
-//  marketTypeArray : ["WIN","PLACE"],
-//  countryCodeArray : ["GB","US","IE"],
-//  eventTypeIDArray : ["7"],
-//  closedMarketDestinationDirectory : '/home/phil/Betfair/NodejsStreamData/Market/ClosedMarketFiles/'
-  marketTypeArray : ["WIN"],
-  countryCodeArray : ["GB"],
-  eventTypeIDArray : ["4339"],
+  marketTypeArray : ["WIN","PLACE"],
+  countryCodeArray : ["GB","US","IE"],
+  eventTypeIDArray : ["7"],
+  //marketTypeArray : ["WIN"],
+  //countryCodeArray : ["GB"],
+  //eventTypeIDArray : ["4339"],
   streamFieldsArray : ["EX_ALL_OFFERS","EX_TRADED","EX_TRADED_VOL","EX_LTP","EX_MARKET_DEF","EX_BEST_OFFERS_DISP","SP_PROJECTED"]
 };
 
